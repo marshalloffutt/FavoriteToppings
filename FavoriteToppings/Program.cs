cd0c4c42997a3a2439d7ea77df390abb537bd463
Marshall Offutt <marshalloffutt@gmail.com>
2019-03-20 19:44:45 -0500

    finally got the damn thing working

--- FavoriteToppings/Program.cs
+++ FavoriteToppings/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace FavoriteToppings
 {
@@ -6,7 +10,30 @@ namespace FavoriteToppings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var allThePizzas = File.ReadAllText(@"../../../pizzas.json");
+            List<Pizza> allTheOrders = JsonConvert.DeserializeObject<List<Pizza>>(allThePizzas);
+
+            //Convert the pizzas
+            var convertedPizzas = allTheOrders.Select(x => x.ToString());
+
+            //Group the pizzas
+            var groupedPizzas = convertedPizzas.GroupBy(x => x);
+
+            //Order them descending
+            var orderedPizzas = groupedPizzas.OrderByDescending(x => x.Count());
+
+            //Assign them to key-value-pairs
+            var keyedPizzas = orderedPizzas.Select(x => new { x.Key, Quantity = x.Count() });
+
+            //The top 20 orders
+            var gimmeTwenty = keyedPizzas.Take(20);
+
+            foreach (var pizza in gimmeTwenty)
+            {
+                Console.WriteLine($"{pizza.Key} has been ordered {pizza.Quantity} times.");
+            }
+
+            Console.ReadKey();
         }
     }
-}
+};
